48fefb0b7833670143343d5287df4db4b2e927c1
ibrahim7176 <iahmed781@yahoo.com>
2022-08-21 02:35:01 -0800

    profile page without posts

--- instaclone/resources/views/home.blade.php
+++ instaclone/resources/views/home.blade.php
@@ -1,1 +1,33 @@
 @extends('layouts.navbar')
+@section('content')
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://mdbootstrap.com/img/new/avatars/1.jpg" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div class="d-flex justify-content-between align-items-baseline">
+                <div class="d-flex align-items-center pb-3">
+                    <div class="h4">user Name</div>
+                    
+                </div>
+                <a href="">Add New Post</a>
+            </div>
+            
+            
+                <a href="">Edit Profile</a>
+            
+
+            <div class="d-flex">
+                <div class="pr-5"><strong>8</strong> posts</div>
+                <div class="pr-5"><strong>87</strong> followers</div>
+                <div class="pr-5"><strong>728</strong> following</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    <div class="row pt-5">
+        {{-- add posts of user --}}
+    </div>
+</div>
+@endsection 
--- instaclone/resources/views/layouts/navbar.blade.php
+++ instaclone/resources/views/layouts/navbar.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-</head>    
+</head>  
+<body>  
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <!-- Container wrapper -->
     <div class="container-fluid">
@@ -76,6 +77,10 @@
       </div>
     </div>
   </nav>
+  <div class="container">
+    @yield('content')
+  </div>
+</body>
   
 
 <script src="https://kit.fontawesome.com/caa6082b62.js" crossorigin="anonymous"></script>
